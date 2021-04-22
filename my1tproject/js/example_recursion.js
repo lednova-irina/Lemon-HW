759632d9fd54992bea8203bcf55cb1e4657d3e93
Irene <irene.lednev@gmail.com>
2021-04-22 21:01:53 +0300

    recursion examples

---
+++ my1tproject/js/example_recursion.js
@@ -0,0 +1,26 @@
+function calcFactorialWithWhile(value) {
+    let result = 1;
+    while (value > 0) {
+        result *= value--;
+    }
+    return result;
+}
+console.warn(calcFactorialWithWhile(6));
+
+function calcFactorialWithFor(value) {
+    let result;
+    for (result = 1; value > 0; value--) {
+        result *= value;
+    }
+    return result;
+}
+console.warn(calcFactorialWithFor(6));
+
+function calcFactorialWithRecursion(value) {
+    if (value <= 1) {
+        return value;
+    }
+    return value * calcFactorialWithRecursion(--value)
+}
+
+console.warn(calcFactorialWithRecursion(6));
